1d23dc5eb3dab7b053d97b2a75f7252b4d49c879
Aditya Kumar <aditya00447@gmail.com>
2026-08-16 16:29:54 +0530

    bravo

--- backend/src/config/mail.config.js
+++ backend/src/config/mail.config.js
@@ -1,28 +1,22 @@
-import { brevo } from '@getbrevo/brevo'
+import { BrevoClient } from "@getbrevo/brevo";
+import { ENV } from "./env.config.js";
 
-const apiInstance = new brevo.TransactionalEmailsApi();
+const brevoClient = new BrevoClient({
+    apiKey: ENV.BREVO_API_KEY
+});
 
-apiInstance.setApiKey(
-    brevo.TransactionalEmailsApiApiKeys.apiKey,
-    process.env.BREVO_API_KEY
-);
-
-export const sendEmail = async ({ to, subject, html }) => {
-    const email = new brevo.SendSmtpEmail();
-
-    email.sender = {
-        name: "Nexify",
-        email: "your-verified-email@example.com"
-    };
-
-    email.to = [
-        {
-            email: to
-        }
-    ];
-
-    email.subject = subject;
-    email.htmlContent = html;
-
-    return await apiInstance.sendTransacEmail(email);
+export const sendEmail = async ({ from, to, subject, html }) => {
+    return await brevoClient.transactionalEmails.sendTransacEmail({
+        sender: {
+            email: from,
+            name: "Nexify"
+        },
+        to: [
+            {
+                email: to
+            }
+        ],
+        subject,
+        htmlContent: html
+    });
 };
